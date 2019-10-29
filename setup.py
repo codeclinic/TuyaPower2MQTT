9538ea234f9527f2350e8b00f81e5f127828a5da
Phill Healey [Code Clinic KreativAgentur] <phill@codeclinic.de>
2019-10-29 23:37:36 +0100

    PyPi Revisions 0.2.1
    Tweaks for PyPi release

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 setup(
   name = 'tuyapower2mqtt',         # How you named your package folder (MyLib)
   packages = ['tuyapower2mqtt'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Reports all the power stats & on/off state from a Tuya / SmartLife / Jinvoo WiFI smart devices such as a socket or switch including newer v3.3 protocol devices',   # Give a short description about your library
-  long_description_content_type='text/markdown' # Use readme.md instead of readme.rst
   author = 'Phill Healey',                   # Type in your name
   author_email = 'phill@codeclinic.de',      # Type in your E-Mail
   url = 'https://github.com/codeclinic/TuyaPower2MQTT',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/codeclinic/TuyaPower2MQTT/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/codeclinic/TuyaPower2MQTT/archive/0.2.1.tar.gz',    # I explain this later on
   keywords = ['TUYA', 'POWER', 'STATS', 'DATA', 'REPORTING', 'SWITCH', 'SOCKET', 'OUTLET', 'SMARTLIFE', 'JINVOO', 'WIFI', 'SMARTDEVICE', 'SMART', 'HOME','AUTOMATION'],   # Keywords that define your package best
   install_requires=[            # dependencies
           'pycrypto',
@@ -17,9 +16,10 @@ setup(
           'paes',
           'pytuya',
           'paho-mqtt',
+          'datetime',
       ],
   classifiers=[
-    'Development Status :: 4 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
